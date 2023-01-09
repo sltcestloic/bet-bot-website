120e2c0229040ba1d4d4e81ad452b083de3a2544
wshloic <lbertran@student.42lyon.fr>
2023-01-09 15:27:47 +0100

    add examples

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,9 +22,41 @@ function Home() {
                 <li className='text-[#9B9D9F] flex gap-3 items-center'><i class="fa-solid fa-check text-[#63F58C]"></i>Classement des meilleurs parieurs</li>
                 <li className='text-[#9B9D9F] flex gap-3 items-center'><i class="fa-solid fa-check text-[#63F58C]"></i>Statistiques et profils d'utilisateurs</li>
             </ul>
+			<div className='h-1 bg-[#3D3E48] w-1/4'></div>
         </section>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#3994FF" fill-opacity="1" d="M0,288L48,272C96,256,192,224,288,197.3C384,171,480,149,576,165.3C672,181,768,235,864,250.7C960,267,1056,245,1152,250.7C1248,256,1344,288,1392,304L1440,320L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
-        <section className='text-white'></section>
+        
+		<section className='text-white text-center'>
+
+			<div className='flex flex-col gap-8 items-center p-14'>
+				
+				<div className='flex items-center gap-10 text-[#9B9D9F]'>
+					<p className='max-w-sm'>Lance un paris sur le match de ton choix du sport de ton choix parmis les sports supportés, les côtes et les scores seront mis à jours automatiquement et le paris se terminera en même temps que le match</p>
+					<img className="max-w-sm" src="bet-example.png" alt='ouais'/>
+				</div>
+				
+			</div>
+
+			<div className='flex flex-col gap-8 items-center p-14'>
+				
+				<div className='flex items-center gap-10 text-[#9B9D9F]'>
+					<img className="max-w-sm" src="result-example.png" alt='ouais'/>
+					<p className='max-w-sm'>À la fin du match, les résultats sont annoncés avec la liste des gains et des pertes, ainsi que les sommes et les côtes des paris placés</p>
+				</div>
+				
+			</div>
+
+			<div className='flex flex-col gap-8 items-center p-14'>
+				
+				<div className='flex items-center gap-10 text-[#9B9D9F]'>
+					<p className='max-w-sm'>Plusieurs classements sont disponibles en fonction de différentes statistiques, des classements mensuels et des classement par compétition et par sports sont à venir !</p>
+					<img className="max-w-sm" src="ladder-example.png" alt='ouais'/>
+				</div>
+				
+			</div>
+
+		</section>
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#3994FF" fill-opacity="1" d="M0,288L48,272C96,256,192,224,288,197.3C384,171,480,149,576,165.3C672,181,768,235,864,250.7C960,267,1056,245,1152,250.7C1248,256,1344,288,1392,304L1440,320L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+
     </div>
   )
 }
